68fe8e257aeeef49a1d09d92ed0f089cbb366115
Aaron Bellenoit <aaronbellenoit@Xenos.local>
2018-10-25 21:50:27 -0400

    [Commiting Android Studio UI code to tnw git repository.]

---
+++ SmartKitchenTableRemote/app/src/main/java/com/thenightswatch/smartkitchentableremote/OptionsMenu.java
@@ -0,0 +1,13 @@
+package com.thenightswatch.smartkitchentableremote;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OptionsMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_options_menu);
+    }
+}
---
+++ SmartKitchenTableRemote/app/src/main/java/com/thenightswatch/smartkitchentableremote/remote_control.java
@@ -0,0 +1,19 @@
+package com.thenightswatch.smartkitchentableremote;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class remote_control extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_remote_control);
+    }
+
+    /** Called when the user taps the Options button */
+    public void goToOptionsMenu(View view) {
+        // Do something in response to the button
+    }
+}
