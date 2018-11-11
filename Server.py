393a5f72041a43466f1ed7475b973338b9d2f131
Nate Eaddie <nate@Nates-MacBook-Pro.local>
2018-11-11 12:16:25 -0500

    port 3 -> port_any

--- Server.py
+++ Server.py
@@ -1,8 +1,13 @@
 import bluetooth*
 
 server_socket = BluetoothSocket(RFCOMM)
-server_socket.bind(("",3))
+server_socket.bind(("",PORT_ANY))
 server_socket.listen(1)
+
+port = server_sock.getsockname()[1]
+
+#identical UUID for connection to client
+uuid = "a1bb5f8d-406d-4119-9cc8-f6c8395514ae"
 
 client_socket, address = server_socket.accept()
 
@@ -12,6 +17,11 @@ print "recieved[%s]" % data
 
 client_socket.close()
 server_socket.close()
+
+
+
+if __name__ == '__main__':
+    main()
 
 
 
