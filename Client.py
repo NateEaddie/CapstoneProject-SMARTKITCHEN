00b3352238d1e7f38acc535c55b6eb6ef49057f5
Nate Eaddie <nate@Nates-MacBook-Pro.local>
2018-11-11 11:35:30 -0500

    Simple client program

---
+++ Client.py
@@ -0,0 +1,17 @@
+import bluetooth*
+
+server_socket = BluetoothSocket(RFCOMM)
+server_socket.bind(("",3))
+server_socket.listen(1)
+
+client_socket, address = server_socket.accept()
+
+data = client_socket.recv(1024)
+
+print "recieved[%s]" % data
+
+client_socket.close()
+server_socket.close()
+
+
+
