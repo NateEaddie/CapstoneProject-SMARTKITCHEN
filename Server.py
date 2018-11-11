7f7a17cd86c13c6faae9382f9e1f18b4e1e5d5e5
brodey11 <bd948215@ohio.edu>
2018-11-11 12:06:50 -0500

    reformatted and added variables

--- Server.py
+++ Server.py
@@ -1,12 +1,17 @@
 import bluetooth*
 
+hostMACAddress = '00:1f:e1:dd:08:3d' # The MAC address of a Bluetooth adapter on the server. The server might have multiple Bluetooth adapters.
+
+port = 3 # 3 is an arbitrary choice. However, it must match the port used by the client.
+size = 1024
+
 server_socket = BluetoothSocket(RFCOMM)
-server_socket.bind(("",3))
+server_socket.bind(("",port)) #the quote can likely be replaced with the hostMACAdress
 server_socket.listen(1)
 
 client_socket, address = server_socket.accept()
 
-data = client_socket.recv(1024)
+data = client_socket.recv(size)
 
 print "recieved[%s]" % data
 
