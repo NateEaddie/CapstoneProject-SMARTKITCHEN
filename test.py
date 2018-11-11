51fefb9f24edf81157b977ee7a39aa166dcdedd7
Nate Eaddie <nate@Nates-MacBook-Pro.local>
2018-11-11 11:34:06 -0500

    test to establish bt connection

---
+++ test.py
@@ -0,0 +1,22 @@
+import bluetooth 
+
+"""
+Establish a connection to a bluetooth device
+
+"""
+
+target_name = "Nate's iPhone"
+target_address = "9C:F4:8E:E7:34:D0"
+
+nearby_devices = bluetooth.discover_devices()
+
+for bdaddr in nearby_devices:
+    if target_name == bluetooth.lookup_name( bdaddr ):
+        target_address = bdaddr
+        break
+
+if target_address is not None:
+    print "found target bluetooth device with address ", target_address
+else:
+    print "could not find target bluetooth device nearby"
+
